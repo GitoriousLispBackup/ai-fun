c967c29f6e851c048155c2dcc4ebbfb8ea218b4d
Tudor M. P <clapautius@gmail.com>
2009-11-09 11:52:33 +0200

    Implemented 'board' class and rewritten print-board method in an OO-style.
    Implemented a generic 'board' class with some basic functionality. Also
    implemented print-board method to display a board on standard output using ASCII
    characters.

--- x-and-0.lisp
+++ x-and-0.lisp
@@ -1,32 +1,77 @@
+
+;;; board class
+
+(defclass board ()
+  ((size
+	:initarg :size
+	:initform (error "Must supply board size")
+	:reader size)
+
+   ;; :fixme: - probably not needed
+   (first-is-black
+	:initarg :first-is-black
+	:initform t
+	:reader first-is-black)
+
+   ;; some games use the intersection (go), others use the spaces in the grid (x-and-0)
+   ;; this property is used when the board is displayed
+   (point-at-intersection
+	:initarg :point-at-intersection
+	:initform t
+	:reader point-at-intersection)
+
+   ;; will be initialized by the initialize-instance (see below)
+   ;; :todo: - add accessor
+   (board-array
+	:initform nil)))
+
+
+(defmethod initialize-instance :after ((brd board) &key)
+  (setf (slot-value brd 'board-array)
+		(make-array (list (size brd) (size brd)) :initial-element nil)))
+
+(defgeneric print-board (board)
+  (:documentation "Print a game board at the standard output using ASCII chars"))
+
+;;; end board class
+
+  
 (defun parse-move (board move mark)
   (let ((x (/ (- move (mod move 10)) 10)) 
 	(y (mod move 10)))
     (if (and (< x 3) (< y 3) 
-	 (eql (aref board x y) nil)) 
-	(setf (aref board x y) mark))))
+	 (eql (aref (slot-value board 'board-array) x y) nil)) 
+	(setf (aref (slot-value board 'board-array) x y) mark))))
 
 (defun prompt-move (player)
   (format *query-io* "~%~a's move: " player)
   (force-output *query-io*)
   (read-line *query-io*))
 
-(defun print-board (board)
-  (dotimes (i 3)
-    (format t " .---.---.---. ~% | ")
-    (dotimes (j 3)
-      (if (eql (aref board i j) nil)
-	  (format t " ")
-	  (format t "~a" (aref board i j)))
-      (format t " | "))
-    (format t "~%"))
-  (format t " '---'---'---'"))
-
-(defun board-init (size)
-  (defvar board nil)
-  (setf board (make-array (list size size) :initial-element nil)))
+(defmethod print-board ((brd board))
+  (flet ( (print-sep-line (width)
+			  (format t ".")
+			  (dotimes (j width)
+				(format t "-."))
+			  (format t "~%")))
+	(if (point-at-intersection brd)
+		(error "Not implemented yet.")
+		(progn
+		  ;; first line
+		  (print-sep-line (size brd))
+		  ;; main content
+		  (dotimes (i (size brd))
+			(format t "|")
+		  (dotimes (j (size brd))
+			(if (aref (slot-value brd 'board-array) i j)
+				(format t "~a" (aref (slot-value brd 'board-array) i j))
+				(format t " "))
+			(format t "|"))
+		  (format t "~%")
+		  (print-sep-line (size brd)))))))
 
 (defun board-game-run (board)
-  (dotimes (i (array-total-size board))
+  (dotimes (i (array-total-size (slot-value board 'board-array)))
     (print-board board)
     (let ((player 0) (mark 0))
       (if (eql (mod i 2) 0)

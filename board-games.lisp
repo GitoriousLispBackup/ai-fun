dc8867c45a43b7cc0e6ac99ded32f340fb6b9389
Tudor M. P <clapautius@gmail.com>
2009-11-09 21:52:40 +0200

    Moved generic game classes/functions in a separate file (board-games.lisp).

---
+++ board-games.lisp
@@ -0,0 +1,83 @@
+;;; board class
+
+;;;(in-package :board-games) ;; :todo:
+
+(defclass board ()
+  ((size
+	:initarg :size
+	:initform (error "Must supply board size")
+	:reader size)
+
+   ;; :fixme: - probably not needed
+   (first-is-black
+	:initarg :first-is-black
+	:initform t
+	:reader first-is-black)
+
+   ;; Some games use the intersection (go), others use the spaces in the grid
+   ;; (x-and-0)
+   ;; This property is used when the board is displayed
+   (point-at-intersection
+	:initarg :point-at-intersection
+	:initform t
+	:reader point-at-intersection)
+
+   ;; Will be initialized by the initialize-instance (see below)
+   ;; :todo: - add accessor
+   (board-array
+	:initform nil)))
+
+
+(defmethod initialize-instance :after ((brd board) &key)
+  (setf (slot-value brd 'board-array)
+		(make-array (list (size brd) (size brd)) :initial-element nil)))
+
+;;; end board class
+
+
+(defgeneric print-board (board)
+  (:documentation "Print a game board at the standard output using ASCII
+  chars"))
+
+(defmethod print-board ((brd board))
+  (flet ( 
+		 (print-sep1-line (width)
+		   (format t "-")
+		   (dotimes (j width)
+			 (format t "-"))
+		   (format t "-~%"))
+
+		 (print-sep2-line (width)
+		   (format t ".")
+		   (dotimes (j width)
+			 (format t "-."))
+		   (format t "~%")))
+	(if (point-at-intersection brd)
+		;; if point is at intersection (eg. go)
+		(progn
+		  ;; first line
+		  (print-sep1-line (size brd))
+		  ;; main content
+		  (dotimes (i (size brd))
+			(format t "|")
+			(dotimes (j (size brd))
+			  (if (aref (slot-value brd 'board-array) i j)
+				  (format t "~a" (aref (slot-value brd 'board-array) i j))
+				  (format t ".")))
+			(format t "|~%"))
+		  ;; last line
+		  (print-sep1-line (size brd)))
+		;; else - point in grid space (e.g. x-and-0)
+		(progn
+		  ;; first line
+		  (print-sep2-line (size brd))
+		  ;; main content
+		  (dotimes (i (size brd))
+			(format t "|")
+		  (dotimes (j (size brd))
+			(if (aref (slot-value brd 'board-array) i j)
+				(format t "~a" (aref (slot-value brd 'board-array) i j))
+				(format t " "))
+			(format t "|"))
+		  (format t "~%")
+		  (print-sep2-line (size brd)))))))
--- x-and-0.lisp
+++ x-and-0.lisp
@@ -1,41 +1,11 @@
+;;;(use-package :board-games) ;; :todo:
 
-;;; board class
+(defclass x-and-0-board (board)
+  ( (size
+	 :initform 3)
+	(point-at-intersection
+	 :initform nil)))
 
-(defclass board ()
-  ((size
-	:initarg :size
-	:initform (error "Must supply board size")
-	:reader size)
-
-   ;; :fixme: - probably not needed
-   (first-is-black
-	:initarg :first-is-black
-	:initform t
-	:reader first-is-black)
-
-   ;; some games use the intersection (go), others use the spaces in the grid (x-and-0)
-   ;; this property is used when the board is displayed
-   (point-at-intersection
-	:initarg :point-at-intersection
-	:initform t
-	:reader point-at-intersection)
-
-   ;; will be initialized by the initialize-instance (see below)
-   ;; :todo: - add accessor
-   (board-array
-	:initform nil)))
-
-
-(defmethod initialize-instance :after ((brd board) &key)
-  (setf (slot-value brd 'board-array)
-		(make-array (list (size brd) (size brd)) :initial-element nil)))
-
-(defgeneric print-board (board)
-  (:documentation "Print a game board at the standard output using ASCII chars"))
-
-;;; end board class
-
-  
 (defun parse-move (board move mark)
   (let ((x (/ (- move (mod move 10)) 10)) 
 	(y (mod move 10)))
@@ -48,29 +18,7 @@
   (force-output *query-io*)
   (read-line *query-io*))
 
-(defmethod print-board ((brd board))
-  (flet ( (print-sep-line (width)
-			  (format t ".")
-			  (dotimes (j width)
-				(format t "-."))
-			  (format t "~%")))
-	(if (point-at-intersection brd)
-		(error "Not implemented yet.")
-		(progn
-		  ;; first line
-		  (print-sep-line (size brd))
-		  ;; main content
-		  (dotimes (i (size brd))
-			(format t "|")
-		  (dotimes (j (size brd))
-			(if (aref (slot-value brd 'board-array) i j)
-				(format t "~a" (aref (slot-value brd 'board-array) i j))
-				(format t " "))
-			(format t "|"))
-		  (format t "~%")
-		  (print-sep-line (size brd)))))))
-
-(defun board-game-run (board)
+(defun x-and-0-run (board)
   (dotimes (i (array-total-size (slot-value board 'board-array)))
     (print-board board)
     (let ((player 0) (mark 0))
